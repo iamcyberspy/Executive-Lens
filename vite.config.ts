45a1afb827e0420541060d64236d84a9d1553c90
iamcyberspy <98733007+iamcyberspy@users.noreply.github.com>
2026-04-22 09:55:17 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(({mode}) => {
-  base: '/Executive-Lens/',
+export default defineConfig(({mode}) => {  
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/Executive-Lens/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
